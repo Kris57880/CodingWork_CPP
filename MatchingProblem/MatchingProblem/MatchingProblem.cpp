595eb1c1d92b3c4ab8b041a8e28330ad3e2eb461
Kris57880 <45487107+Kris57880@users.noreply.github.com>
2018-12-01 23:48:34 +0800

    MatchingProblem

---
+++ MatchingProblem/MatchingProblem/MatchingProblem.cpp
@@ -0,0 +1,23 @@
+// MatchingProblem.cpp : Defines the entry point for the console application.
+//Author Hiko
+//Gale-Shapley Algorithm(n^2 )
+/*-----------------------------------------------------------------
+	intialize each person to be free
+	While(some man m is free and hasn't propose to every woman)do
+		w=heightst ranked woman in m's list to whom m hasn't yet prposed;
+		if(w is free)
+			(m,w)become engaged;
+		else if(w prefers mto her fiance m')
+			(m,w)become engaged;
+			m' become free;
+	return the set S of engaged pairs
+-------------------------------------------------------------------*/
+//https://zerojudge.tw/ShowProblem?problemid=c484
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
