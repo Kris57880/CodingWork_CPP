e0812e560c671dfbabe27f114523f5ccab1d2bba
Kris57880 <45487107+Kris57880@users.noreply.github.com>
2018-12-10 13:04:01 +0800

    Update MatchingProblem.cpp

--- MatchingProblem/MatchingProblem/MatchingProblem.cpp
+++ MatchingProblem/MatchingProblem/MatchingProblem.cpp
@@ -15,10 +15,37 @@
 //https://zerojudge.tw/ShowProblem?problemid=c484
 #include "stdafx.h"
 #include <iostream>
+#include <vector>
 using namespace std;
+void input(vector<vector<int> >&vec);
+void output();
 int main()
 {
-	cout << "helloworld" << endl;
-    return 0;
+	//配置
+	vector<vector<int> > p_M;//preferance of Man
+	vector<vector<int> > p_W;//preferance of Woman
+	vector<int> r_M;//Result of Man
+	vector<int> r_W;//Result of Woman 
+	//初始化 
+	p_M.resize(10);
+	p_W.resize(10);
+	for(int i=0;i!=10;i++){
+		p_M[i].resize(10);
+		p_W[i].resize(10);
+	}
+	r_M.resize(10,-1);
+	r_W.resize(10,-1);
+	//輸入
+	input(p_M);
+	input(p_W);
+	//配對
+	
+	 
 }
-
+void input(vector<vector<int> >&vec){
+	for(int i=0;i!=10;i++){
+		for(int j=0;j!=10;j++)
+			cin>>vec[i][j];
+	}
+}
+void output();
